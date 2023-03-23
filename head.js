ef83994bf76a24d635def1a5d1bc22198b3f4897
Raymond Lin <raymond.lin.7690@gmail.com>
2023-03-23 15:16:14 -0700

    final version of head.js

--- head.js
+++ head.js
@@ -1,13 +1,13 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`${actual}`);
+    console.log(`✅✅✅Assertation passed: ${actual} === ${expected}`);
   } else {
-    console.log(`${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑Assertation failed: ${actual} !== ${expected}`);
   }
 };
-const head = function(array){
+const head = function(array) {
   return array[0];
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello","Lighthouse", "Labs"]),"Hellos");
