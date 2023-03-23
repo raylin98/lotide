a0c5fa7a093b161e99f32f448d0bc3c6b741df84
Raymond Lin <raymond.lin.7690@gmail.com>
2023-03-23 13:18:25 -0700

    fixed script to console.log the actual element

--- head.js
+++ head.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Heads`);
+    console.log(`${actual}`);
   } else {
-    console.log(`undefined`);
+    console.log(`${actual} !== ${expected}`);
   }
 };
 const head = function(array){
@@ -10,4 +10,4 @@ const head = function(array){
 }
 
 assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello","Lighthouse", "Labs"]),"Hello");
+assertEqual(head(["Hello","Lighthouse", "Labs"]),"Hellos");
