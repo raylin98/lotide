58ea753007c1d1243488e107c7dcffedab8baf37
Raymond Lin <raymond.lin.7690@gmail.com>
2023-03-25 17:25:28 -0700

    updated arrayequal with refined eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,21 +6,16 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
-  }
-  return true;
-}
+  } return true;
+};
 
-const assertArraysEqual = function (arra1,arra2 ) {
-  if (eqArrays(arra1,arra2)=== true) {
+const assertArraysEqual = function(arra1,arra2) {
+  if (eqArrays(arra1,arra2) === true) {
     console.log('Arrays are the same');
   } else {
     console.log('The arrays are not the same');
