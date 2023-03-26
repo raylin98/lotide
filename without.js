b2dbf6abbdad95673a50683bf48c4b88a97b149b
Raymond Lin <raymond.lin.7690@gmail.com>
2023-03-25 17:03:29 -0700

    eslinted

--- without.js
+++ without.js
@@ -1,4 +1,4 @@
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   } else {
@@ -9,26 +9,26 @@ const eqArrays = function (array1, array2) {
     }
   }
   return true;
-}
+};
 
-const assertArraysEqual = function (arra1,arra2 ) {
-  if (eqArrays(arra1,arra2)=== true) {
+const assertArraysEqual = function(arra1,arra2) {
+  if (eqArrays(arra1,arra2) === true) {
     console.log('Arrays are the same');
   } else {
     console.log('The arrays are not the same');
   }
 };
 
-const without = function (source,itemsToRemove) {
+const without = function(source,itemsToRemove) {
   for (let i = 0; i < source.length; i++) {
-    for ( let j = 0; j < itemsToRemove.length; j++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
       if (source[i] === itemsToRemove[j]) {
         source.splice(i,1);
       }
     }
   }
   return source;
-}
+};
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
@@ -37,4 +37,4 @@ assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 
 console.log(without([1, 2, 3], [2])); // => [1, 3]
-console.log(without(["1", "2", "3", "4"], [1, 2, "3", "4"])) // => ["1", "2"]
+console.log(without(["1", "2", "3", "4"], [1, 2, "3", "4"])); // => ["1", "2"]
