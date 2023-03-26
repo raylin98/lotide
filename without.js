cb574ef2559ed04e6ef63c2bbfaf9910be699ff2
Raymond Lin <raymond.lin.7690@gmail.com>
2023-03-25 17:26:38 -0700

    fixed without function with updated eqArrays function

--- without.js
+++ without.js
@@ -1,14 +1,9 @@
 const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
-  }
-  return true;
+  } return true;
 };
 
 const assertArraysEqual = function(arra1,arra2) {
