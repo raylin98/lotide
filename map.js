2a0af5f0d9c17df023a34ae3adb26b1e70863ac8
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-02 11:41:30 -0700

    linted

--- map.js
+++ map.js
@@ -7,26 +7,26 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  if(array1.length !== array2.length) 
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  } return true;
+  if (array1.length !== array2.length)
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    } return true;
 };
 
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function (array, callback){
+const map = function(array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
   }
   return results;
-}
+};
 
-const results1= map(words, word => word[0]);
-console.log(results1)
+const results1 = map(words, word => word[0]);
+console.log(results1);
 
 assertEqual(eqArrays(results1, ['g', 'c', 't', 'm', 't']), true);
 assertEqual(eqArrays(results1, ['g', 'c', 't', 'm']), false);
