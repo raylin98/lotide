82a214b44043b49a8b77e333b1face48f928a54d
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-12 15:53:01 -0700

    refactored middleTest.js for mocha chai testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,16 @@
-const assertArraysEqual = require(`../assertArraysEqual`)
+const assert = require('chai').assert;
 const middle = require(`../middle`);
 
+describe("#middle", () => {
+ it("returns [2] for [1,2,3]",() => {
+    assert.deepEqual(middle([1,2,3]),[2]);
+  });
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1,2,3,4,5,6])); // => [3,4]
-console.log(middle([1,2,3,4,5,6,7,8])); // => [4,5]
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]),[2,3]);
+  });
+
+  it("returns [3] for [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+});
