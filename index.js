52a28b2578402fc63fbfa88b448e7fb224905f31
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-12 16:10:45 -0700

    exported some of the functions

---
+++ index.js
@@ -0,0 +1,18 @@
+const head = require('./head');
+
+const tail = require('./tail');
+
+const middle = require ('./middle');
+
+const assertArraysEqual = require('./assertArraysEqual');
+
+const assertEqual = require('./assertEqual');
+
+const assertObjectsEqual = require('./assertObjectsEqual');
+
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+};
