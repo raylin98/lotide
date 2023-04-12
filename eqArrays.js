1c5021b82cedee666deca19084dd6f15f9a641cc
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-12 14:44:13 -0700

    finally fixed eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,18 @@
 const assertEqual = require(`./assertEqual`);
 
 const eqArrays = function(array1, array2) {
-  if(array1.length && array2.length) 
+  if (!Array.isArray(array2)) {
+    return false;
+  }
+  if (array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
     }
-  } return true;
+  }
+  return true;
 };
 
 module.exports = eqArrays
