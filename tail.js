7b0b4db2e2ea35ec0308e3e10307197a6e864e32
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-12 14:10:54 -0700

    refactored tail.js to export the function

--- tail.js
+++ tail.js
@@ -1,20 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertation passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertation failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require(`./assertEqual`);
 
 const tail = function(array) {
   return array.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-/*tests*/
-assertEqual(words.length, 2);
-assertEqual(words[0], "Yo Yo");
-assertEqual(words[0], "Labs");
-assertEqual(words[2],"Lighthouse")
+
+
+module.exports = tail
