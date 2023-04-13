daa08394372f51cce951eabf87342add3ac72440
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-12 21:01:18 -0700

    refactored and exported findKeyByValue function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertation passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertation failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const findKeyByValue = function(object, value) {
   for (const key in object) {
@@ -14,6 +8,9 @@ const findKeyByValue = function(object, value) {
   return undefined
 };
 
+module.exports = findKeyByValue;
+
+/*
 const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
@@ -22,3 +19,4 @@ const bestTVShowsByGenre = {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+*/
