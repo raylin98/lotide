1eec3ce655d8a8a0c0f498b99e7bd7b9967f3201
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-12 20:49:41 -0700

    exported letterPositions function

--- letterPositions.js
+++ letterPositions.js
@@ -1,26 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertation passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertation failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  } return true;
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(arra1,arra2) {
-  if (eqArrays(arra1,arra2) === true) {
-    console.log('Arrays are the same');
-  } else {
-    console.log('The arrays are not the same');
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const letterPositions = function(sentence) {
   const results = {};
@@ -36,6 +18,8 @@ const letterPositions = function(sentence) {
   return results;
 }
 
-  assertArraysEqual(letterPositions("hello").e, [1]);
+module.exports = letterPositions;
 
-  console.log(letterPositions("Lighthouse in the House"));
+/* assertArraysEqual(letterPositions("hello").e, [1]);
+
+console.log(letterPositions("Lighthouse in the House")); */
