519a9ac6b51e01df67eab7a6e344e28648f1c29d
Raymond Lin <raymond.lin.7690@gmail.com>
2023-04-12 20:51:49 -0700

    refactored and exported map function

--- map.js
+++ map.js
@@ -1,19 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertation passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertation failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length)
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    } return true;
-};
+const eqArrays = require('./eqArrays');
 
 const words = ["ground", "control", "to", "major", "tom"];
 
@@ -25,9 +12,11 @@ const map = function(array, callback) {
   return results;
 };
 
-const results1 = map(words, word => word[0]);
+module.exports = map;
+
+/* const results1 = map(words, word => word[0]);
 console.log(results1);
 
 assertEqual(eqArrays(results1, ['g', 'c', 't', 'm', 't']), true);
 assertEqual(eqArrays(results1, ['g', 'c', 't', 'm']), false);
-assertEqual(eqArrays(results1, ['G', 'C', 'T', 'M', 'T']), false);
+assertEqual(eqArrays(results1, ['G', 'C', 'T', 'M', 'T']), false); */
